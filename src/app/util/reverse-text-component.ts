abe3ba6fab41322347177dbd5a76ba168312f531
Alfred Obialo <alfredcsdinc@gmail.com>
2022-10-12 07:32:04 +0100

    Add reverse-text-component.ts

---
+++ src/app/util/reverse-text-component.ts
@@ -0,0 +1,27 @@
+﻿import {Component} from "@angular/core";
+
+
+@Component({
+    template : `
+        <h2>Reverse Text App</h2>
+        <input type="text" name="txt" [(ngModel)]="txt">
+        
+        <p style="color:#1e55ff; font-weight:700;">{{reverseText(txt)}}</p>
+    `,
+    selector :"v-reverse-text"
+    
+})
+export class ReverseTextComponent
+{
+    txt:string = "";
+   
+    reverseText(input : string )
+    {
+        let result = "";
+        for( let i = 0 ; i < input.length; i++)
+        {
+           result = input[i] + result;
+        }
+        return  result;
+    }
+}
